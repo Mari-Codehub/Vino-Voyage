262ae5ef9bb0e1090aff308e880f60c1b069e1a5
gocha <marykuprashvili@gmail.com>
2023-03-18 15:59:24 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".gallery__carousel").owlCarousel({
+    items: 4,
+    loop: true,
+    autoplay: false,
+    dots: false,
+    nav: false,
+    margin: 30,
+  });
+});
