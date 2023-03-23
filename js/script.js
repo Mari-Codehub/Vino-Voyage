638fc2f7ad6ac080580b3588c8a8628371cb23b4
Mari-Codehub <127653210+Mari-Codehub@users.noreply.github.com>
2023-03-23 16:50:54 +0400

    correcting mistakes and add responsive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
 $(document).ready(function () {
   $(".gallery__carousel").owlCarousel({
-    items: 4,
+    items: 6,
     loop: true,
     autoplay: false,
     dots: false,
     nav: false,
     margin: 30,
+    responsive: {
+      1600: {
+        items: 4,
+      },
+      1200: {
+        items: 3,
+      },
+      768: {
+        items: 1,
+      },
+      320: {
+        items: 1,
+      },
+    },
   });
+});
+
+let burgerIcon = document.getElementById("burgerIcon");
+let burgerMenu = document.getElementById("burgerMenu");
+
+let menuVisible = false;
+
+burgerIcon.addEventListener("click", function () {
+  if (menuVisible) {
+    burgerMenu.style.display = "none";
+    menuVisible = false;
+  } else {
+    burgerMenu.style.display = "block";
+    menuVisible = true;
+  }
 });
